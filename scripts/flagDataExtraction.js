738f311614556a3433b26d22e8d63e6989078143
divyabiyani <divyabiyani26@gmail.com>
2017-04-14 00:56:06 +0530

    Css updated

--- scripts/flagDataExtraction.js
+++ scripts/flagDataExtraction.js
@@ -4,26 +4,19 @@ var dataExtract = function() {
   $.ajax({
     url: "https://restcountries.eu/rest/v2/all?fields=name;latlng;capital",
     // Handle as Text
-    dataType: "text",
+    dataType: "json",
     success: function(data) {
-    // Parse JSON file
-    var json = $.parseJSON(data);
-    //Store data into a variable
-    // Display Players
-    console.log('dataExtract');
-    dataInsert(json);
+    dataInsert(data);
     }
   });
 }
 
 var dataInsert = function(json) {
-  console.log('dataInsert');
   var length = Object.keys(json).length;
   var dataIns = "<ul>";
 
   for(var i=0;i<length;i++) {
-    console.log(json[i].flag);
-    dataIns += '<li><div class="box effect1">' + json[i].name +'<br/><img src=' + json[i].flag + ' alt=' + json[i].name +'><br/>' + json[i].latlng[0] + ', ' + json[i].latlng[1] + '<div class="overlay"><summary><h2>' + json[i].name + '</h2><h3>Capital: ' + json[i].capital +  '</h3><h3>Latitude: ' + json[i].latlng[0] + '</h3><h3>Longitute: ' + json[i].latlng[1] +'</summary></div></div></li>';
+    dataIns += '<li><div class="box effect1"><strong>' + json[i].name.toUpperCase() +'</strong><br/><div class="image-effect"><img src=' + json[i].flag + '><br/></div>' + json[i].latlng[0] + ', ' + json[i].latlng[1] + '<div class="overlay"><summary><h2>' + json[i].name + '</h2><h3>Capital: ' + json[i].capital +  '</h3><h3>Latitude: ' + json[i].latlng[0] + '</h3><h3>Longitute: ' + json[i].latlng[1] +'</summary></div></div></li>';
   }
   dataIns += '</ul>';
   $("#our-work").append(dataIns);
